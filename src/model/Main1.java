a49dfbf2a5ec3072c21a5fa1776e0f3100330057
FernandaRojas152 <FernandaRojas152@hotmail.com>
2020-09-17 21:54:56 -0500

    code accepted by online judge.

--- src/model/Main1.java
+++ src/model/Main1.java
@@ -3,10 +3,8 @@ package model;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 
 /**
@@ -17,9 +15,6 @@ import java.util.HashMap;
  */
 public class Main1 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		/** HashMap of Java that represents the group of trees */
-		HashMap<String, Integer> tree;
-
 		/** 
 		 * It's used BufferedReader instead of Scanner to read the data because BufferedReader has
 		 * more memory than a Scanner, so it is useful for long strings(which is this case)
@@ -31,60 +26,39 @@ public class Main1 {
 		/** The amount of test cases that are going to happen*/
 		int cases= Integer.parseInt(br.readLine());
 
-		/** An integer that represents the one hundred percent, the total of trees*/
-		final int PERCENTAGE= 100;
-
-		/** Amount of times a key it's appearing*/
-		int rep=0;
 
 		/** The first line, that it's going to be the empty line*/
-		String line= br.readLine();
+		String line=br.readLine();
 
-		/** An integer that represents the total of trees that the user inputs*/
-		int size= 0;
 		/**
 		 * pre:
 		 * pos:
 		 */
 		for (int i = 0; i < cases; i++) {
-			tree=new HashMap<String, Integer>();
+			HashMap<String, Integer> tree=new HashMap<String,Integer>();
+			ArrayList<String> a= new ArrayList<>();
+			double size= 0;
 			while((line= br.readLine())!=null && !line.isEmpty()) {
-				if(tree.containsKey(line)) {
-					rep= tree.get(line);
-					tree.put(line, rep+1);
-				}else {
-					tree.put(line, 1);
+				if(!tree.containsKey(line)) {
+					a.add(line);
 				}
+				tree.put(line, tree.getOrDefault(line,0)+1);
 				size++;
 			}
 			/**
-			 * how to loop in hashMap? Map.Entry<K,V>
+			 * how to loop in hashMap? Map.Entry<K,V> m: tree.entrySet()
 			 * entrySet() shows all the maps, set viewing of the mappings in the map (API)
 			 */
-			/**for(Map.Entry<String, Integer> me : tree.entrySet()) {
+			Collections.sort(a);
+			StringBuilder sb=new StringBuilder();
+			for(String t : a) {
 				//shows 4 decimals it's used %.4f o DecimalFormat d = new DecimalFormat("#.####");
-				String key = me.getKey();
-				Integer total = me.getValue();
-				double p= (total*1.0/size)*PERCENTAGE;
-
-				System.out.printf("%s %.4f\n", key, p);
-				//System.out.println(key+ " "+ df.format(p));
-			}*/
-
-			ArrayList<String> a= new ArrayList<>(tree.keySet());
-			Collections.sort(a, new Comparator<String>() {
-				@Override
-				public int compare(String o1, String o2 ) {
-					return o1.compareTo(o2);
-				}
-			});
-			for(int j=0; j<a.size(); j++) {
-				int total= tree.get(a.get(j));
-				double p= (total*1.0/size)*PERCENTAGE;
-				System.out.printf("%s %.4f\n", a.get(j), p);
+				sb.append(String.format("%s %.4f\n", t, (tree.get(t)/size)*100.0));
 			}
 			if (i<cases-1) {
-				System.out.println(" ");
+				System.out.println(sb.toString());
+			}else {
+				System.out.print(sb.toString());
 			}
 		}
 	}
